cb2b2def0fd5e834d3fdc7dff3eca4a71bf6a3ed
LukaLuxx <luka.jambrosic18@gmail.com>
2023-01-08 17:55:30 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUser] = useState([
+        {
+            ime: "Branko",
+            prezime: "Branković",
+            dob: 32,
+        },
+        {
+            ime: "Janko",
+            prezime: "Janković",
+            dob: 42,
+        },
+        {
+            ime: "Marko",
+            prezime: "Marković",
+            dob: 52,
+        },
+    ]);
+    return (
+        <div className="App">
+            {users.map((user) => (
+                <h1>
+                    {" "}
+                    <hr />
+                    Ime:{user.ime} <br />
+                    Prezime:{user.prezime} <br />
+                    Dob:{user.dob} <hr />
+                </h1>
+            ))}
+        </div>
+    );
 }
 
 export default App;
